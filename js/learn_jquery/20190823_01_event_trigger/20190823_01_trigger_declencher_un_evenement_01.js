34b754ca555254ad9636ba7782cca579e6461689
sekaherve@gmail.com <guyancourt07>
2019-08-23 01:01:17 +0200

    fix Wording

--- js/learn_jquery/20190823_01_event_trigger/20190823_01_trigger_declencher_un_evenement_01.js
+++ js/learn_jquery/20190823_01_event_trigger/20190823_01_trigger_declencher_un_evenement_01.js
@@ -1,7 +1,7 @@
 /**
  * Created by Herve on 10/08/2019.
  */
-// Désactiver une méthode de gestion évènementielle :
+// Déclencher un évènement grâce au "trigger" :
 $(function() {
     $('img').click(function() {
         $('#message').html("L'image a été cliquée").fadeIn(1000).fadeOut(1000);
