253085a96e3a9470cf81d0b014b76f486cc93096
rvck2007 <sekaherve@gmail.com>
2020-11-14 06:05:27 +0100

    Exemple #3 Promesses

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
 // DECLARATION :
-const calcul = new Promise((resolve, reject) => {
-    // ...
-    const result = 200 * 120;
+const calcul = (num1, num2) => {
+    return new Promise((resolve, reject) => {
+        // ...
+        const result = num1 * num2;
 
-    if(result) {
-        resolve(result);
-    } else {
-        reject();
-    }
-})
-
+        if(result > 100000) {
+            resolve(result);
+        } else {
+            reject("Résultat trop petit");
+        }
+    })
+}
 
 
 // UTILISATION :
-calcul.then((result) => {
+calcul(300, 1000).then((result) => {
     console.log('Resultat: ' + result);
-}).catch(() => {
-    console.log('Oups, une erreur !')
+}).catch((err) => {
+    console.log('Error : ' + err);
 })
